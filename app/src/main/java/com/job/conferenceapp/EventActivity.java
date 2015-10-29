c2d1357d41858ba8bbd44cdc31bff212c6730320
JobM <jobmwaniki18@gmail.com>
2015-10-29 21:05:43 +0300

    added tabular view

---
+++ app/src/main/java/com/job/conferenceapp/EventActivity.java
@@ -0,0 +1,18 @@
+package com.job.conferenceapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Job on 10/29/2015.
+ */
+public class EventActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_items);
+    }
+
+
+}
+
---
+++ app/src/main/java/com/job/conferenceapp/TabFragment2.java
@@ -0,0 +1,16 @@
+package com.job.conferenceapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by Job on 10/29/2015.
+ */
+public class TabFragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+    }
+}
