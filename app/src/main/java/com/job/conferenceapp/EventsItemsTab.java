95f34b899f15e7ffee7d47a554589c8b72ca8e0e
JobM <jobmwaniki18@gmail.com>
2015-10-30 11:44:52 +0300

    tab layout view working

---
+++ app/src/main/java/com/job/conferenceapp/EventsItemsTab.java
@@ -0,0 +1,55 @@
+package com.job.conferenceapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+/**
+ * Created by Job on 10/29/2015.
+ */
+public class EventsItemsTab extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_item_tab);
+        //recycler view
+        RecyclerView rv = (RecyclerView)findViewById(R.id.card_view);
+        rv.setHasFixedSize(true);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Speakers"));
+        tabLayout.addTab(tabLayout.newTab().setText("Agenda"));
+        tabLayout.addTab(tabLayout.newTab().setText("Twitter"));
+        tabLayout.addTab(tabLayout.newTab().setText("Info"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+
+}
